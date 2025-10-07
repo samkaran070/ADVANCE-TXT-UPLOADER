6097729b57b4e1a0add091c4729518e605885aa1
samkaran070 <freemovieslogin@gmail.com>
2025-10-07 15:54:37 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,6 +3,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "25961729")) #Replace with your api id
-API_HASH = environ.get("API_HASH", "d969691a3e1e5d8387d63d2fcfd5dcb5") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "8475690707:AAE6AaUg89MgB00mtoTUXKZxy4-jYT556yw") #Replace with your bot token
+API_ID = int(environ.get("API_ID", "2129")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "d96fd5dcb5") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "8475690Zxy4-jYT556yw") #Replace with your bot token
